8fdbfc49c8551ec18f77245b61a006deb160af7a
Evan Hsu <evanhsu@gmail.com>
2015-11-29 17:38:13 -0800

    Added HTML5 geolocation to the Helicopter Status Update form

--- resources/views/helicopters/new_status.blade.php
+++ resources/views/helicopters/new_status.blade.php
@@ -66,6 +66,12 @@
                             <span class="input-group-addon">'</span>
                         </span>
                     </div>
+            <br />
+                    <div class="form-group">
+                        <div class="col-offset-sm-2 col-offset-md-3">
+                            <a href="#" id="geolocate_button">Use current location</a>
+                        </div>
+                    </div>
              
             </div>
             
@@ -155,5 +161,45 @@
     </div>
     
 </div>
+
+@endsection
+
+
+@section('scripts-postload')
+@parent
+
+<script>
+    (function() {
+        // Add geolocation trigger to the 'geolocate_button'
+        $('#geolocate_button').click(function() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                // Location successfully found
+                populatePositionFields(position.coords.latitude, position.coords.longitude);
+
+            }); // End .getCurrentPosition()
+        }); // End .click()
+    })();
+
+    function populatePositionFields(latitude,longitude) {
+        // Coordinate input format is Decimal-degrees with "Easting" longitudes (locations in the western hemisphere have negative longitude).
+        // i.e. latitude = 42.389043    longitude = -120.87849
+        // Convert this into "westing" decimal-minutes and populate the form with the resulting values (western hemisphere has positive longitude).
+        var lat_deg = Math.floor(Math.abs(latitude));
+        var lat_min = (Math.abs(latitude) - lat_deg) * 60.0;
+        lat_deg = latitude >= 0 ? lat_deg : (lat_deg * 1.0);
+
+        var lon_deg = Math.floor(Math.abs(longitude)); // -122.5 -> +122
+        var lon_min = (Math.abs(longitude) - lon_deg) * 60.0;
+        lon_deg = longitude < 0 ? lon_deg : (lon_deg * -1.0); // Switch from 'East' reference to 'West' reference (invert sign)
+
+        $('#latitude_deg').val( lat_deg );
+        $('#latitude_min').val( lat_min );
+
+        $('#longitude_deg').val( lon_deg );
+        $('#longitude_min').val( lon_min );
+
+    } // End populatePositionFields()
+
+</script>
 
 @endsection
