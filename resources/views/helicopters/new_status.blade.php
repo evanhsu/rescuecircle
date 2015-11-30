814be71b88a995844b6405109e14fbf301ff8ffd
Evan Hsu <evanhsu@gmail.com>
2015-11-30 10:11:23 -0800

    Limited decimal precision for geolocation lat/lon on the Helicopter Status update form

--- resources/views/helicopters/new_status.blade.php
+++ resources/views/helicopters/new_status.blade.php
@@ -184,19 +184,20 @@
         // Coordinate input format is Decimal-degrees with "Easting" longitudes (locations in the western hemisphere have negative longitude).
         // i.e. latitude = 42.389043    longitude = -120.87849
         // Convert this into "westing" decimal-minutes and populate the form with the resulting values (western hemisphere has positive longitude).
-        var lat_deg = Math.floor(Math.abs(latitude));
-        var lat_min = (Math.abs(latitude) - lat_deg) * 60.0;
-        lat_deg = latitude >= 0 ? lat_deg : (lat_deg * 1.0);
 
-        var lon_deg = Math.floor(Math.abs(longitude)); // -122.5 -> +122
+        var lat_deg = Math.floor(Math.abs(latitude));   // Use absolute value so that floor() works as desired for negative numbers
+        var lat_min = (Math.abs(latitude) - lat_deg) * 60.0;
+        lat_deg = latitude >= 0 ? lat_deg : (lat_deg * 1.0); // Restore the original sign
+
+        var lon_deg = Math.floor(Math.abs(longitude)); // Use absolute value so that floor() works as desired for negative numbers
         var lon_min = (Math.abs(longitude) - lon_deg) * 60.0;
         lon_deg = longitude < 0 ? lon_deg : (lon_deg * -1.0); // Switch from 'East' reference to 'West' reference (invert sign)
 
         $('#latitude_deg').val( lat_deg );
-        $('#latitude_min').val( lat_min );
+        $('#latitude_min').val( toPrecision(lat_min,6) );
 
         $('#longitude_deg').val( lon_deg );
-        $('#longitude_min').val( lon_min );
+        $('#longitude_min').val( toPrecision(lon_min,6) );
 
     } // End populatePositionFields()
 
